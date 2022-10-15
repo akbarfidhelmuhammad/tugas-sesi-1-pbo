298549dc1acbddda72067fe215d1b988fe23073a
akbarfidhelmuhammad <114375080+akbarfidhelmuhammad@users.noreply.github.com>
2022-10-15 09:25:21 +0700

    Add files via upload

---
+++ Tugassesi1pbono2.java
@@ -0,0 +1,22 @@
+package Tugassesi1pbono2;
+import javax.swing.JOptionPane;
+
+// Nama  : akbar fidhel muhammad
+// Kelas : TO21A
+// Nim : 20210040052
+
+public class Tugassesi1pbono2 {
+    public static void main(String[] args) {
+    //1
+    JOptionPane.showMessageDialog(null, "Belajar Java");
+    JOptionPane.showMessageDialog(null, "Sangat Mudah Sekali");
+    JOptionPane.showMessageDialog(null, "dan Sangat Menyenangkan");
+    
+    //2
+    String input = JOptionPane.showInputDialog("Matkul apa yang anda pelajari");
+    JOptionPane.showMessageDialog(null, "Belajar" + input + "sangat mudah.");
+        
+               
+    }
+    
+}
